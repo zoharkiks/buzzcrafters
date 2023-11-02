a43ebe2aa02425871ce25a0bea8a8aa07b8c244b
Zohar Williams <zoharkiks@gmail.com>
2023-11-02 14:25:25 +0530

    Add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily:{
-				'sans': ['Space Grotesk', 'sans'],
-
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Space Grotesk", "sans"],
+      },
+      colors: {
+        "surface-primary": "#2F2F2F",
+        "surface-secondary": "#FEFEFC",
+        "surface-tertiary": "#F7F7F7",
+        "accent-primary": "#FD6A13",
+        "accent-secondary": "#2F2F2D",
+        "text-primary": "#F5F0EA",
+        "text-secondary": "#000000",
+      },
+    },
+  },
+  plugins: [],
+};
